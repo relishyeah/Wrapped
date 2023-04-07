0f3fd86ecc67d3b2af81d34f8ca0700abfd3d59a
Quinn Relyea <quinnrelyea@gmail.com>
2023-04-07 13:07:00 -0700

    Removed old bow CSS, tweaked landing page

--- wrapped/src/pages/Home/Home.tsx
+++ wrapped/src/pages/Home/Home.tsx
@@ -4,17 +4,24 @@ import './styles.css';
 const Home = () => {
     return (
     <div className='container'>
-        <div className='title'>
-            Wrapped, <br/> Wrapped
-            <img src='https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg' />
-        </div>
-        
-        <br></br>
-        
-
-        <div className='logIn'>
-                Unwrap yours
+        <div className="content">
+            <div className='title'>
+                Wrapped, <br/> Wrapped
+                <img src='https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg' />
             </div>
+            
+            <div className="tagline">
+                Unwrap your listening, year by year
+            </div>
+            
+
+            <div onClick={() => console.log(123)} className='logIn'>
+                    See yours
+            </div>
+        </div>
+        <div className="about">
+            Created by Quinn Relyea | About | Privacy
+        </div>
     </div>  );
 }
  
